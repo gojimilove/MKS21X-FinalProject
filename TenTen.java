0d9e439598bedff9590015df6a6e36fb46b7dab6
Jessica Kim <jkim00@stuy.edu>
2019-01-12 17:32:52 -0500

    clearing functions and documentation (hong wei only)

--- TenTen.java
+++ TenTen.java
@@ -3,6 +3,7 @@ public class TenTen {
     private Piece[] pieces;
     private int score, count;
 
+    //creates a new, empty 10x10 board (array of Tiles), resets the score, preps pieces
     public TenTen() {
       //board is 10x10 array of empty tiles, initialize tiles
       board = new Tile[10][10];
@@ -18,10 +19,12 @@ public class TenTen {
       count = 0;
     }
 
+    //fills a single tile on the board
     private void fillTile(int row, int col) {
       board[row][col].fillTile();
     }
 
+    //tests if entire row is filled (will later be cleared)
     private boolean rowFilled(int row) {
       //checks if all tiles in the
     	for (int i = 0; i < board.length; i++) {
@@ -30,6 +33,7 @@ public class TenTen {
       return true;
     }
 
+    //tests if the column is completely filled (will later be cleared)
     private boolean columnFilled(int col) {
       for (int i = 0; i < board.length; i++) {
         if (!board[i][col].isFilled()) return false;
@@ -37,6 +41,7 @@ public class TenTen {
       return true;
     }
 
+    //tests if the piece "fits" in a specific spot, whether it overlaps anything
     private boolean pieceFits(Piece x, int row, int col) {
       //if the piece goes out of bounds return false
       if (row+x.length() > board.length || col+x.length() > board[0].length) return false;
@@ -49,18 +54,30 @@ public class TenTen {
     	return true;
     }
 
+    //clearing a row when entirely filled
     private void clearRow(int row) {
-
+			for (int i = 0; i < board[row].length; i++) {
+        board[row][i].clearTile();
+      }
     }
 
+    //clearing a column when entirely filled
     private void clearColumn(int col) {
-
+      for (int i = 0; i < board.length; i++){
+        board[i][col].clearTile();
+      }
     }
 
+    //clear the whole board
     private void clear() {
-
+      for (int i = 0; i < board.length; i++) {
+        for (int j = 0; j < board[i].length; j++) {
+          board[i][j].clearTile();
+        }
+      }
     }
 
+    //spawns the 3 pieces next to the board that the player can select
     private void spawnPieces() {
       for (int i = 0; i < pieces.length; i++) {
         pieces[i] = new Piece();
@@ -69,6 +86,7 @@ public class TenTen {
 
     }
 
+    //puts the piece on the board if it fits
     private void addPiece(Piece x, int row, int col){
       //if the piece fits fill in the tiles on the board where piece would go
       if (pieceFits(x, row, col)) {
@@ -81,14 +99,17 @@ public class TenTen {
       }
     }
 
+    //returns the current score
     private int getScore() {
     	return score;
     }
 
+    //increases score, whether it be by clearing pieces, placing pieces, getting combos
     private void addPoints(int added) {
     	score+= added;
     }
 
+    //resets the score
     private void clearPoints() {
     	score = 0;
     }
