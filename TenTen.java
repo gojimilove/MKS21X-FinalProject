33ac5a613486494bbed3e46fc993908effd80467
Jessica Kim <jkim00@stuy.edu>
2019-01-14 00:45:49 -0500

    scoring system works, can also clear columns/rows by filling them with pieces, also added documentation and instructions/side notes for players

--- TenTen.java
+++ TenTen.java
@@ -73,6 +73,7 @@ public class TenTen {
 			for (int i = 0; i < board[row].length; i++) {
         board[row][i].clearTile();
       }
+      score+=10;
     }
 
     //clearing a column when entirely filled
@@ -80,6 +81,7 @@ public class TenTen {
       for (int i = 0; i < board.length; i++){
         board[i][col].clearTile();
       }
+      score+=10;
     }
 
     //clear the whole board
@@ -113,6 +115,7 @@ public class TenTen {
           }
         }
       }
+      score+=(x.length()*x.length());
     }
 
     //returns the current score
@@ -143,6 +146,7 @@ public class TenTen {
       return s;
     }
 
+    //number of pieces player can select that are not on the board
     public int piecesWaiting(){
       return count;
     }
@@ -156,79 +160,97 @@ public class TenTen {
 
     public static void main(String[] args) {
 
-    int x = 0;
-    int y = 7;
+      int x = 0;
+      int y = 8;
+      //create terminal
+      Terminal terminal = TerminalFacade.createTextTerminal();
+      terminal.enterPrivateMode();
 
-    Terminal terminal = TerminalFacade.createTextTerminal();
-    terminal.enterPrivateMode();
+      TerminalSize size = terminal.getTerminalSize();
+      terminal.setCursorVisible(false);
+      boolean running = true;
 
-    TerminalSize size = terminal.getTerminalSize();
-    terminal.setCursorVisible(false);
+      TenTen a = new TenTen();
+      a.spawnPieces(terminal);
+      Piece help = new Piece(2);
 
-    boolean running = true;
+      while(running){
+        //set up cursor
+        terminal.moveCursor(x,y);
+        //terminal.applyBackgroundColor(Terminal.Color.WHITE);
+        terminal.applyForegroundColor(Terminal.Color.RED);
+        //applySGR(a,b) for multiple modifiers (bold,blink) etc.
+        terminal.putCharacter('\u00a4');
+        //terminal.putCharacter(' ');
+        //terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
+        terminal.applyForegroundColor(Terminal.Color.DEFAULT);
+        //terminal.applySGR(Terminal.SGR.RESET_ALL);
 
-    TenTen a = new TenTen();
-    
-    
-    
-    a.spawnPieces(terminal);
+        Key key = terminal.readInput();
 
-    Piece help = new Piece();
-
-    while(running){
-
-      terminal.moveCursor(x,y);
-      //terminal.applyBackgroundColor(Terminal.Color.WHITE);
-      terminal.applyForegroundColor(Terminal.Color.RED);
-      //applySGR(a,b) for multiple modifiers (bold,blink) etc.
-      terminal.putCharacter('\u00a4');
-      //terminal.putCharacter(' ');
-      //terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
-      terminal.applyForegroundColor(Terminal.Color.DEFAULT);
-      //terminal.applySGR(Terminal.SGR.RESET_ALL);
-
-      Key key = terminal.readInput();
-
-      if (key != null){
-
-        if (key.getKind() == Key.Kind.Escape) {
-          terminal.exitPrivateMode();
-          running = false;
-        }
-
-        if (key.getKind() == Key.Kind.ArrowLeft) {
-          terminal.moveCursor(x,y);
-          terminal.putCharacter(' ');
-          x--;
-        }
-
-        if (key.getKind() == Key.Kind.ArrowRight) {
-          terminal.moveCursor(x,y);
-          terminal.putCharacter(' ');
-          x++;
-        }
-
-        if (key.getKind() == Key.Kind.ArrowUp) {
-          terminal.moveCursor(x,y);
-          terminal.putCharacter(' ');
-          y--;
-        }
-
-        if (key.getKind() == Key.Kind.ArrowDown) {
-          terminal.moveCursor(x,y);
-          terminal.putCharacter(' ');
-          y++;
-        }
-        if (key.getCharacter() == '1') {
-          if (a.pieceFits(help, x-1, y-9)) {
-            a.addPiece(help, x-1, y-9);
+        if (key != null){
+          //exiting the game
+          if (key.getKind() == Key.Kind.Escape) {
+            terminal.exitPrivateMode();
+            running = false;
           }
+          //moving cursor around
+          if (key.getKind() == Key.Kind.ArrowLeft) {
+            terminal.moveCursor(x,y);
+            terminal.putCharacter(' ');
+            x--;
+          }
+
+          if (key.getKind() == Key.Kind.ArrowRight) {
+            terminal.moveCursor(x,y);
+            terminal.putCharacter(' ');
+            x++;
+          }
+
+          if (key.getKind() == Key.Kind.ArrowUp) {
+            terminal.moveCursor(x,y);
+            terminal.putCharacter(' ');
+            y--;
+          }
+
+          if (key.getKind() == Key.Kind.ArrowDown) {
+            terminal.moveCursor(x,y);
+            terminal.putCharacter(' ');
+            y++;
+          }
+          //selecting pieces to put on the board
+          if (key.getCharacter() == '1') {
+            if (a.pieceFits(help, y-9,x-1)) {
+              a.addPiece(help, y-9, x-1);
+            }
+          }
+
+          if (key.getCharacter() == '2') {
+            if (a.pieceFits(help, y-9,x-1)) {
+              a.addPiece(help, y-9, x-1);
+            }
+          }
+
+          if (key.getCharacter() == '3') {
+            if (a.pieceFits(help, y-9,x-1)) {
+              a.addPiece(help, y-9, x-1);
+            }
+          }
+
         }
+        for (int i = 0; i < 10; i++) {
+          if (a.rowFilled(i)) a.clearRow(i);
+          if (a.columnFilled(i)) a.clearColumn(i);
+        }
+
+        //instructions, shows position, indicates pieces
+        putString(0,0,terminal,"Welcome to our version of TenTen!\nTo place a piece, move the cursor with the arrow keys to where you would like to place the piece.\nThen, click either 1, 2, or 3 to select one of the pieces shown below the board, and if it fits where you tried to place it, it will be placed on the board. You earn points by placing and clearing pieces; when you place a piece, you get the same amount of points as the number of tiles that block takes up. You also recieve 10 points for each row or column you clear. \nNote: a piece is selected from its top left corner ALSO please do not go over the pieces with the cursor.");
+        putString(0,7,terminal,"Current position: ["+x+","+y+"]     ");
+        putString(0,9,terminal,a.toString());
+        putString(0,20,terminal,"Pieces:");
+        putString(0,34,terminal,"Score: "+a.getScore());
+        putString(0,36,terminal,"SIDE NOTES FOR NOW: \nThe pieces that are shown below the board are not the ones being placed on the board as of now since we have not figured out to access them (xd), so 1 2 and 3 place a 2x2 piece instead.\nWe are also working on generating more complicated pieces, instead of just 1x1, 2x2, or 3x3 squares.");
       }
-      putString(0,1,terminal,"Welcome to our version of TenTen!\nTo place a piece, move the cursor with the arrow keys to where you would like to place the piece.\nThen, click either 1, 2, or 3 to select one of the pieces shown below the board, and if it fits where you tried to place it, it will be placed on the board.\nNote: a piece is selected from its top left corner ALSO please do not go over the pieces with the cursor.");
-      putString(0,9,terminal,a.toString());
-      putString(0,20,terminal,"Pieces:");
     }
-  }
 
 }
