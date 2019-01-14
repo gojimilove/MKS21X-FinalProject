b9693197463589b4e4ffd18a535968feb70a10bd
Jessica Kim <jkim00@stuy.edu>
2019-01-13 22:13:41 -0500

    BASIC terminal stuff we now have a character that moves around the terminal

--- TenTen.java
+++ TenTen.java
@@ -1,3 +1,17 @@
+import com.googlecode.lanterna.terminal.Terminal.SGR;
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.Key.Kind;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.Terminal.Color;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.LanternaException;
+import com.googlecode.lanterna.input.CharacterPattern;
+import com.googlecode.lanterna.input.InputDecoder;
+import com.googlecode.lanterna.input.InputProvider;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
+
 public class TenTen {
     private Tile[][] board;
     private Piece[] pieces;
@@ -126,15 +140,75 @@ public class TenTen {
       }
       return s;
     }
+
     public int bnunmb(){
       return count;
     }
+
+    public static void putString(int r, int c,Terminal t, String s){
+      t.moveCursor(r,c);
+      for(int i = 0; i < s.length();i++){
+        t.putCharacter(s.charAt(i));
+      }
+    }
+
     public static void main(String[] args) {
-      TenTen a = new TenTen();
+      /*TenTen a = new TenTen();
       a.addPiece(new Piece(2),0,0);
       a.addPiece(new Piece(3),5,1);
       a.addPiece(new Piece(3),8,8);
       System.out.println(a);
-      System.out.println("Pieces on board: "+a.bnunmb()+"\n");
+      System.out.println("Pieces on board: "+a.bnunmb()+"\n");*/
+
+      int x = 2;
+      int y = 2;
+
+      Terminal terminal = TerminalFacade.createTextTerminal();
+      terminal.enterPrivateMode();
+
+      TerminalSize size = terminal.getTerminalSize();
+      terminal.setCursorVisible(false);
+
+      boolean running = true;
+
+      while (running) {
+        terminal.moveCursor(x,y);
+        terminal.putCharacter('\u00a4');
+        terminal.applyBackgroundColor(Terminal.Color.WHITE);
+        terminal.applyForegroundColor(Terminal.Color.RED);
+
+        Key key = terminal.readInput();
+
+        if (key != null){
+          if (key.getKind() == Key.Kind.Escape) {
+            terminal.exitPrivateMode();
+            running = false;
+          }
+
+          if (key.getKind() == Key.Kind.ArrowLeft) {
+            terminal.moveCursor(x,y);
+            terminal.putCharacter(' ');
+            x--;
+          }
+
+          if (key.getKind() == Key.Kind.ArrowRight) {
+            terminal.moveCursor(x,y);
+            terminal.putCharacter(' ');
+            x++;
+          }
+
+          if (key.getKind() == Key.Kind.ArrowUp) {
+            terminal.moveCursor(x,y);
+            terminal.putCharacter(' ');
+            y--;
+          }
+
+          if (key.getKind() == Key.Kind.ArrowDown) {
+            terminal.moveCursor(x,y);
+            terminal.putCharacter(' ');
+            y++;
+          }
+        }
+      }
     }
 }
